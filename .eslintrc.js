2742fc68c7b212a5e855c545f2de4b25788c6e61
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-10-16 22:57:20 +0200

    finish first small seerver with middleware

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12
+  },
+  plugins: [
+    "react"
+  ],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"]
+  }
+};
